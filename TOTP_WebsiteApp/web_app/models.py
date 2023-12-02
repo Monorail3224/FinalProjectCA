0b06a73b47cb0a32a5c4be76ecdb780ede79792a
Monorail3224 <callen_12@mail.fhsu.edu>
2023-12-02 17:07:26 -0600

    Modified models.py and added urls.py

--- TOTP_WebsiteApp/web_app/models.py
+++ TOTP_WebsiteApp/web_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    # Add profile fields like full name, profile picture, etc.
+
+class TwoFactorAuthSettings(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    totp_secret_key = models.CharField(max_length=16)  # Store the TOTP secret key securely
+
+class UserLog(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    action = models.CharField(max_length=255)  # Describe the user's action, e.g., login, logout
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class AccessToken(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)  # Store the access token
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# Additional fields for User model or a custom user model if needed
+# class CustomUser(AbstractUser):
+#     # Add custom fields here if necessary
+#     pass
---
+++ TOTP_WebsiteApp/web_app/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # User Registration and Authentication
+    path('api/register/', views.register_user, name='register_user'),
+    path('api/login/', views.login_user, name='login_user'),
+    path('api/logout/', views.logout_user, name='logout_user'),
+
+    # TOTP Setup and Verification
+    path('api/totp/setup/', views.setup_totp, name='setup_totp'),
+    path('api/totp/verify/', views.verify_totp, name='verify_totp'),
+    path('api/totp/reset/', views.reset_totp, name='reset_totp'),
+
+    # User Profile Management
+    path('api/profile/', views.get_profile, name='get_profile'),
+    path('api/profile/update/', views.update_profile, name='update_profile'),
+
+    # Password and 2FA Settings Management
+    path('api/password/reset/', views.reset_password, name='reset_password'),
+    path('api/2fa/', views.manage_2fa_settings, name='manage_2fa_settings'),
+
+    # Admin User Management and Logs
+    path('api/users/', views.manage_users, name='manage_users'),
+    path('api/logs/', views.view_logs, name='view_logs'),
+
+    # Token Management
+    path('api/token/', views.generate_token, name='generate_token'),
+
+    # API Versioning (if needed)
+    # path('api/v1/register/', views.register_user, name='v1_register_user'),
+    # path('api/v1/login/', views.login_user, name='v1_login_user'),
+    # ...
+
+    # Add more endpoints as needed for your project
+]
