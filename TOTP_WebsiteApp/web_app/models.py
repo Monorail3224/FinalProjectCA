8025622f250f28c107b526979d5cacf6ace02bfd
Monorail3224 <callen_12@mail.fhsu.edu>
2023-12-03 16:21:41 -0600

    Refined models.py and improved index.html

--- TOTP_WebsiteApp/web_app/models.py
+++ TOTP_WebsiteApp/web_app/models.py
@@ -1,25 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class UserProfile(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+class CustomUser(AbstractUser):
+    # Add custom fields to the user model
+    date_of_birth = models.DateField(null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=255, blank=True)
+
     # Add profile fields like full name, profile picture, etc.
 
-class TwoFactorAuthSettings(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    totp_secret_key = models.CharField(max_length=16)  # Store the TOTP secret key securely
+    class Meta:
+        # Add a unique constraint to avoid clashes with auth.User
+        unique_together = ('username', 'email')
+
+    # Specify custom related_name for groups and user_permissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_users',
+        blank=True,
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_users',
+        blank=True,
+        verbose_name='user permissions'
+    )
+
+    def __str__(self):
+        return self.username  # Customize the string representation as needed
+
+class TOTPSetting(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    secret_key = models.CharField(max_length=16, unique=True)  # Store the TOTP secret key securely
+
+    def __str__(self):
+        return f"{self.user.username}'s TOTP Setting"
 
 class UserLog(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     action = models.CharField(max_length=255)  # Describe the user's action, e.g., login, logout
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class AccessToken(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     token = models.CharField(max_length=255)  # Store the access token
     timestamp = models.DateTimeField(auto_now_add=True)
-
-# Additional fields for User model or a custom user model if needed
-# class CustomUser(AbstractUser):
-#     # Add custom fields here if necessary
-#     pass
