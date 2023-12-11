5daa3a111ee177abdd24291909f8f66cb631be77
Monorail3224 <callen_12@mail.fhsu.edu>
2023-12-11 14:30:12 -0600

    Fixed issue with SQL Queries in Views

--- TOTP_WebsiteApp/web_app/views.py
+++ TOTP_WebsiteApp/web_app/views.py
@@ -7,39 +7,62 @@ from django.contrib.auth import login
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
-from .forms import CustomUserCreationForm, LoginForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import PasswordEntry
+from .models import UserProfile
 from .utils import send_sms
 
 # Model to create a new user
 class SignUpView(CreateView):
-    model = User  # Use the built-in User model
-    form_class = CustomUserCreationForm  # Use the custom form
+    model = User
+    form_class = CustomUserCreationForm  # Use the updated form with the 'phone_number' field
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
     def form_valid(self, form):
-        # Call the parent class's form_valid method to save the user
         response = super().form_valid(form)
 
+        # Get the phone number from the form
+        phone_number = form.cleaned_data.get('phone_number')
+
+        # Create a UserProfile for the user and associate it
+        user_profile, created = UserProfile.objects.get_or_create(user=self.object)
+        user_profile.phone_number = phone_number
+        user_profile.save()
+
         # Send an SMS after successful signup
-        user = self.object  # The newly created user
-
-        # Fetch the phone number from the form for the user who just signed up
-        user_phone_number = form.cleaned_data.get('phone_number')
-
         message = "Welcome to my Password Manager app! Thanks for signing up. We will take great care in managing your passwords. >:)"
-        send_sms(user_phone_number, message)
+        send_sms(phone_number, message)
 
         # Log the user in after signup
-        login(self.request, user)
+        login(self.request, self.object)
 
         return response
 
 class CustomLoginView(LoginView):
-    template_name = 'registration/profile.html'  # Replace with the path to your login template
-    success_url = reverse_lazy('profile')  # Replace with the URL to redirect to after login
+    template_name = 'registration/profile.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Fetch the logged-in user
+        user = self.request.user
+
+        # Try to get the associated phone number based on user_id
+        try:
+            user_profile = UserProfile.objects.get(user_id=user.id)
+            user_phone_number = user_profile.phone_number
+        except UserProfile.DoesNotExist:
+            user_phone_number = None
+
+        # Check if a phone number exists for the user and send an SMS alert
+        if user_phone_number:
+            message = "Someone has signed into your account. If this was not you, please contact support immediately."
+            send_sms(user_phone_number, message)
+
+        return response
+
 
 @method_decorator(login_required, name='dispatch')
 class AccountInfoView(LoginRequiredMixin, TemplateView):
