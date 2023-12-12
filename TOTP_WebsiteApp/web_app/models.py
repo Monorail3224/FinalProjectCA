4f61fd1d635e9382b626b218d4353ae322c814cf
Monorail3224 <callen_12@mail.fhsu.edu>
2023-12-11 19:17:11 -0600

    removed password hashing functions

--- TOTP_WebsiteApp/web_app/models.py
+++ TOTP_WebsiteApp/web_app/models.py
@@ -15,17 +15,5 @@ class PasswordEntry(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=128)  # Store hashed passwords securely
 
-    def save(self, *args, **kwargs):
-        # Hash the password before saving it
-        if not self.password.startswith('sha256$'):
-            self.password = make_password(self.password)
-        super(PasswordEntry, self).save(*args, **kwargs)
-
-    def check_password(self, raw_password):
-        """
-        Check if the provided password matches the stored hashed password.
-        """
-        return check_password(raw_password, self.password)
-
     def __str__(self):
         return f"{self.user.username}'s {self.website_name} Password"
